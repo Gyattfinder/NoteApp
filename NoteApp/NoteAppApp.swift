70e115db4010a6415621a20191408664fff35474
Gyattfinder <brent.hoozee@jopie.be>
2025-01-23 11:24:23 +0100

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Brent Hoozee on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
