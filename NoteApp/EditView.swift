6b92fdd8c2f06ff19dc674544fecd999bd5a6b4c
Gyattfinder <brent.hoozee@jopie.be>
2025-02-04 14:05:04 +0100

    nieuwe aanpassingen

---
+++ NoteApp/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  NoteApp
+//
+//  Created by Brent Hoozee on 04/02/2025.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
