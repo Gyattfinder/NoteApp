2777fecfa28cd1b30190625003ca8d953eb7fab6
Gyattfinder <brent.hoozee@jopie.be>
2025-02-04 14:05:28 +0100

    aanpassingen

--- NoteApp/ContentView.swift
+++ NoteApp/ContentView.swift
@@ -7,8 +7,25 @@
 
 import SwiftUI
 
+class NotesView: ObservableObject {
+    @Published var list: [String] = []
+    
+    
+    func addNote(_ note: String) {
+        if !note.isEmpty {
+            list.append(note)
+        }
+    }
+    
+    func deleteItem(at offsets: IndexSet) {
+        list.remove(atOffsets: offsets)
+    }
+}
+
+
 struct ContentView: View {
     @State private var item: String = ""
+    @ObservedObject var viewModel = NotesView()
     var body: some View {
         NavigationStack{
             VStack {
@@ -16,18 +33,43 @@ struct ContentView: View {
                     TextField("Add note", text: $item).padding(.horizontal).padding(.vertical).cornerRadius(20).foregroundColor(.black).textFieldStyle(.roundedBorder)
                     
                     Button(action: {
-                        
+                        viewModel.addNote(item)
+                        item = ""
                     }, label: {
-                        Text("+").padding().fontWeight(.bold).cornerRadius(10).background(.blue).foregroundColor(.white)
+                        Text("+").padding(.horizontal, 20).padding(.vertical,15 ).fontWeight(.bold).background(.blue).foregroundColor(.white).cornerRadius(100)
                     })
                 }
+                
+                List {
+                    ForEach(viewModel.list, id: \.self) { item in
+                        NavigationLink(destination: EditView(task: Binding(
+                            get: { item },
+                            set: { newItem in
+                                if let index = viewModel.list.firstIndex(of: item) {
+                                    viewModel.list[index] = newItem
+                                }
+                            }
+                        ))) {
+                            Text(item)
+                        }
+                    }
+                    .onDelete(perform: viewModel.deleteItem)
+                    
+                }
                 Spacer()
-            }.navigationTitle("Notes")
+            }
+            .navigationTitle("Notes")
+            .navigationBarItems(trailing: Button(action: {
+                
+            }, label: {
+                Text("Edit")
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue)
+            }))
             .padding()
         }
     }
 }
-
 #Preview {
     ContentView()
 }
--- NoteApp/EditView.swift
+++ NoteApp/EditView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct EditView: View {
+    @Binding var task: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Edit task", text: $task)
+                .padding()
+                .textFieldStyle(.roundedBorder)
+                .font(.title)
+            
+            Spacer()
+        }
+        .navigationTitle("Edit Task")
+        .padding()
     }
-}
-
-#Preview {
-    EditView()
 }
